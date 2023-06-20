00edd2a0cecdd6aa1f058682f98e9332c316c1d6
Zehight <1318141712@qq.com>
2023-06-20 20:54:47 +0800

    ok

--- src/views/VoteProject.vue
+++ src/views/VoteProject.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+import logo from '@/assets/logo.png'
 import test from '@/assets/OTLIN01.png'
 
 const tab = ref(1)
@@ -47,23 +48,29 @@ const cardList = [
                 <v-list rounded-0>
                   <v-list-item v-for="item in cardList" :key="item" class="pa-0">
                     <v-card :title="item.title" variant="tonal" class=" ma-2 pa-0">
-                      <v-row class="ma-0">
-                        <v-col cols="2" v-for="role in item.role" :key="role">
-                          <div class="cardArea">
-                            <RoleCard :data-image="test">
-                              <template #header>
-                                <h1> 嘉然Diana</h1>
-                              </template>
-                              <template #content>
-                                <p style="line-height: 20px;margin-top: 20px">
-                                  所属赛区：中国赛区<br>
-                                  所属企划：Asoul
-                                </p>
-                              </template>
-                            </RoleCard>
+                      <div class="d-flex flex-wrap">
+                        <v-col v-for="(role,roleIndex) in item.role" :key="role" cols="12" lg="2" md="3" sm="4" xs="6">
+                          <div class="w-100 position-relative elevation-2 rounded" style="padding-top: 150%;">
+                            <v-img cover class="position-absolute w-100 h-80 pa-2"
+                                   style="top:0;left: 0;right: 0;bottom: 10%"
+                                   v-if="roleIndex%2===1"
+                                   :src="test"></v-img>
+                            <v-img cover class="position-absolute w-100 h-80 pa-2"
+                                   style="top:0;left: 0;right: 0;bottom: 10%"
+                                   v-if="roleIndex%2===0"
+                                   :src="logo"></v-img>
+                            <text cover class="position-absolute w-100 h-80 pa-2"
+                                  style="top:90%;left: 0;right: 0;bottom: 0%"
+                                  v-if="roleIndex%2===0">{{ role }}
+                            </text>
+                            <text cover class="position-absolute w-100 h-80 pa-2"
+                                  style="top:90%;left: 0;right: 0;bottom: 0%"
+                                  v-if="roleIndex%2===1">{{ role }}
+                            </text>
                           </div>
+
                         </v-col>
-                      </v-row>
+                      </div>
                     </v-card>
                   </v-list-item>
                 </v-list>
@@ -82,204 +89,7 @@ const cardList = [
   </TheContainer>
 </template>
 
-<style scoped lang="scss">
-.cardArea {
-  display: flex;
-  flex-wrap: wrap;
-  justify-content: center;
-}
-
-$hoverEasing: cubic-bezier(0.23, 1, 0.32, 1);
-$returnEasing: cubic-bezier(0.445, 0.05, 0.55, 0.95);
-
-body {
-  margin: 40px 0;
-  font-size: 14px;
-  font-weight: 500;
-  background-color: #BCAAA4;
-  -webkit-font-smoothing: antialiased;
-}
-
-.title {
-  font-size: 24px;
-  font-weight: 700;
-  color: #5D4037;
-  text-align: center;
-}
-
-.card-info h1 {
-  font-size: 36px;
-  font-weight: 700;
-  text-shadow: rgba(black, 1) -3px 0px 10px;
-}
-
-p {
-  line-height: 1.5em;
-}
-
-h1 + p, p + p {
-  margin-top: 10px;
-}
-
-.container {
-  padding: 40px 80px;
-  display: flex;
-  flex-wrap: wrap;
-  justify-content: center;
-}
-
-.card-wrap {
-  margin: 10px;
-  transform: perspective(800px);
-  transform-style: preserve-3d;
-  cursor: pointer;
-  // background-color: #fff;
-
-  &:hover {
-    .card-info {
-      transform: translateY(0);
-    }
-
-    .card-info p {
-      opacity: 1;
-    }
-
-    .card-info, .card-info p {
-      transition: 0.6s $hoverEasing;
-    }
-
-    .card-info:after {
-      transition: 5s $hoverEasing;
-      opacity: 1;
-      transform: translateY(0);
-    }
-
-    .card-bg {
-      transition: 0.6s $hoverEasing,
-    }
-
-    .card {
-      transition: 0.6s $hoverEasing,
-      box-shadow 2s $hoverEasing;
-    }
-  }
-}
-
-.normol {
-  position: relative;
-  flex: 0 0 240px;
-  width: 240px;
-  height: 320px;
-  background-color: #333;
-  overflow: hidden;
-  border-radius: 10px;
-  box-shadow: rgba(black, 0.66) 0 3px 6px 0,
-  inset #333 0 0 0 5px,
-  inset rgba(white, 0.3) 0 0 0 6px;
-  transition: 1s $returnEasing;
-}
-
-.selected {
-  position: relative;
-  flex: 0 0 240px;
-  width: 240px;
-  height: 320px;
-  background-color: #333;
-  overflow: hidden;
-  border-radius: 10px;
-  box-shadow: rgba(white, 0.2) 0 3px 6px 0,
-  rgba(white, 1) 0 0 0 5px,
-  rgba(black, 0.66) 0 15px 30px 0,
-  inset #333 0 0 0 5px,
-  inset white 0 0 0 6px;
-  transition: 1s $returnEasing;
-}
-
-
-//.card-bg {
-//  opacity: 0.5;
-//  position: absolute;
-//  top: -20px;
-//  left: -20px;
-//  width: 100%;
-//  height: 100%;
-//  padding: 20px;
-//  background-repeat: no-repeat;
-//  background-position: center;
-//  background-size: cover;
-//  transition: 1s $returnEasing,
-//  opacity 5s 1s $returnEasing;
-//  pointer-events: none;
-//}
-
-.normolBG {
-  opacity: 0.5;
-  position: absolute;
-  top: -20px;
-  left: -20px;
-  width: 100%;
-  height: 100%;
-  padding: 20px;
-  background-repeat: no-repeat;
-  background-position: center;
-  background-size: cover;
-  transition: 1s $returnEasing,
-  opacity 5s 1s $returnEasing;
-  pointer-events: none;
-}
-
-.selectedBG {
-  opacity: 0.9;
-  position: absolute;
-  top: -20px;
-  left: -20px;
-  width: 100%;
-  height: 100%;
-  padding: 20px;
-  background-repeat: no-repeat;
-  background-position: center;
-  background-size: cover;
-  transition: 1s $returnEasing,
-  opacity 5s 1s $returnEasing;
-  pointer-events: none;
-}
-
-.card-info {
-  padding: 20px;
-  position: absolute;
-  bottom: 0;
-  left: 0;
-  right: 0;
-  color: #fff;
-  transform: translateY(50%);
-  transition: 0.6s 1.6s cubic-bezier(0.215, 0.61, 0.355, 1);
-
-  p {
-    opacity: 0;
-    text-shadow: rgba(black, 1) 0 2px 3px;
-    transition: 0.6s 1.6s cubic-bezier(0.215, 0.61, 0.355, 1);
-  }
-
-  * {
-    position: relative;
-    z-index: 1;
-  }
-
-  &:after {
-    content: '';
-    position: absolute;
-    top: 0;
-    left: 0;
-    z-index: 0;
-    width: 100%;
-    height: 100%;
-    background-image: linear-gradient(to bottom, transparent 0%, rgba(#000, 0.6) 100%);
-    background-blend-mode: overlay;
-    opacity: 0;
-    transform: translateY(100%);
-    transition: 5s 1s $returnEasing;
-  }
-}
+<style scoped>
 
 
 </style>
