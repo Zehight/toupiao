01c32395043366595f6be146297a42153876509e
Zehight <1318141712@qq.com>
2023-06-24 19:53:19 +0800

    ok

--- src/views/VoteProject.vue
+++ src/views/VoteProject.vue
@@ -31,54 +31,53 @@ const cardList = [
       <v-btn size="large" elevation="2">添加轮次</v-btn>
     </div>
     <v-window v-model="tab">
-      <v-window-item :value="1">
-        <v-list>
-          <v-list-item
-            v-for="(circleItem,index) in items" :key="index" class=" rounded border-t">
-            <v-list-item-title class="d-flex align-center pa-2">
-              <span class="text-h5">{{ circleItem.title }}</span>
-              <v-btn class="ml-3" elevation="0" variant="outlined">投入角色</v-btn>
-            </v-list-item-title>
-            <v-card elevation="0" class="pa-3 ma-3 ">
-              <v-card-actions class="justify-center">
-                <v-btn variant="tonal">推送</v-btn>
-                <v-btn variant="tonal">冻结</v-btn>
-              </v-card-actions>
-              <template v-slot:text>
-                <v-list rounded-0>
-                  <v-list-item v-for="item in cardList" :key="item" class="pa-0">
-                    <v-card :title="item.title" variant="tonal" class=" ma-2 pa-0">
-                      <div class="d-flex flex-wrap">
-                        <v-col v-for="(role,roleIndex) in item.role" :key="role" cols="12" lg="2" md="3" sm="4" xs="6">
-                          <div class="w-100 position-relative elevation-2 rounded" style="padding-top: 150%;">
-                            <v-img cover class="position-absolute w-100 h-80 pa-2"
-                                   style="top:0;left: 0;right: 0;bottom: 10%"
-                                   v-if="roleIndex%2===1"
-                                   :src="test"></v-img>
-                            <v-img cover class="position-absolute w-100 h-80 pa-2"
-                                   style="top:0;left: 0;right: 0;bottom: 10%"
-                                   v-if="roleIndex%2===0"
-                                   :src="logo"></v-img>
-                            <text cover class="position-absolute w-100 h-80 pa-2"
-                                  style="top:90%;left: 0;right: 0;bottom: 0%"
-                                  v-if="roleIndex%2===0">{{ role }}
-                            </text>
-                            <text cover class="position-absolute w-100 h-80 pa-2"
-                                  style="top:90%;left: 0;right: 0;bottom: 0%"
-                                  v-if="roleIndex%2===1">{{ role }}
-                            </text>
+          <v-window-item :value="1">
+            <v-list>
+              <v-list-item
+                v-for="(circleItem,index) in items" :key="index" class=" rounded border-t">
+                <v-list-item-title class="d-flex align-center pa-2">
+                  <span class="text-h5">{{ circleItem.title }}</span>
+                  <v-btn class="ml-3" elevation="0" variant="outlined">投入角色</v-btn>
+                </v-list-item-title>
+                <v-card elevation="0" class="pa-3 ma-3 ">
+                  <v-card-actions class="justify-center">
+                    <v-btn variant="tonal">推送</v-btn>
+                    <v-btn variant="tonal">冻结addddd</v-btn>
+                  </v-card-actions>
+                  <template v-slot:text>
+                    <v-list rounded-0>
+                      <v-list-item v-for="item in cardList" :key="item" class="pa-0">
+                        <v-card :title="item.title" variant="tonal" class=" ma-2 pa-0">
+                          <div class="d-flex flex-wrap">
+                            <v-col v-for="(role,roleIndex) in item.role" :key="role" cols="12" lg="2" md="3" sm="4" xs="6">
+                              <div class="w-100 position-relative elevation-2 rounded" style="padding-top: 150%;">
+                                <v-img cover class="position-absolute w-100 h-80 pa-2"
+                                       style="top:0;left: 0;right: 0;bottom: 10%"
+                                       v-if="roleIndex%2===1"
+                                       :src="test"></v-img>
+                                <v-img cover class="position-absolute w-100 h-80 pa-2"
+                                       style="top:0;left: 0;right: 0;bottom: 10%"
+                                       v-if="roleIndex%2===0"
+                                       :src="logo"></v-img>
+                                <text cover class="position-absolute w-100 h-80 pa-2"
+                                      style="top:90%;left: 0;right: 0;bottom: 0%"
+                                      v-if="roleIndex%2===0">{{ role }}
+                                </text>
+                                <text cover class="position-absolute w-100 h-80 pa-2"
+                                      style="top:90%;left: 0;right: 0;bottom: 0%"
+                                      v-if="roleIndex%2===1">{{ role }}
+                                </text>
+                              </div>
+                            </v-col>
                           </div>
-
-                        </v-col>
-                      </div>
-                    </v-card>
-                  </v-list-item>
-                </v-list>
-              </template>
-            </v-card>
-          </v-list-item>
-        </v-list>
-      </v-window-item>
+                        </v-card>
+                      </v-list-item>
+                    </v-list>
+                  </template>
+                </v-card>
+              </v-list-item>
+            </v-list>
+          </v-window-item>
       <v-window-item style="height: 100px" :value="2">
         asd
       </v-window-item>
